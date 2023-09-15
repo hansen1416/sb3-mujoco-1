f425c71e3017fc77bdbbabed37d5068a069f51ba
Hanson <105476@grundfos.com>
2023-09-15 16:54:00 +0800

    1

--- PunchEnv.py
+++ PunchEnv.py
@@ -23,3 +23,193 @@ you need a separate test environment to evaluate the performance of your agent a
 It is recommended to periodically evaluate your agent for n test episodes (n is usually between 5 and 20) 
 and average the reward per episode to have a good estimate.
 """
+
+import math
+
+import numpy as np
+import mujoco
+import mujoco.viewer
+from transforms3d import quaternions
+import gymnasium as gym
+from gymnasium import spaces
+from stable_baselines3.common.env_checker import check_env
+from stable_baselines3.common.callbacks import BaseCallback
+from tqdm.auto import tqdm
+
+from xml import arm_xml
+
+
+def normalize(v):
+    norm = np.linalg.norm(v)
+    if norm == 0:
+        return v
+    return v / norm
+
+
+class ProgressBarCallback(BaseCallback):
+    """
+    :param pbar: (tqdm.pbar) Progress bar object
+    """
+
+    def __init__(self, pbar):
+        super().__init__()
+        self._pbar = pbar
+
+    def _on_step(self):
+        # Update the progress bar:
+        self._pbar.n = self.num_timesteps
+        self._pbar.update(0)
+
+# this callback uses the 'with' block, allowing for correct initialisation and destruction
+
+
+class ProgressBarManager(object):
+    def __init__(self, total_timesteps):  # init object with total timesteps
+        self.pbar = None
+        self.total_timesteps = total_timesteps
+
+    def __enter__(self):  # create the progress bar and callback, return the callback
+        self.pbar = tqdm(total=self.total_timesteps)
+
+        return ProgressBarCallback(self.pbar)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):  # close the callback
+        self.pbar.n = self.total_timesteps
+        self.pbar.update(0)
+        self.pbar.close()
+
+
+class PunchEnv(gym.Env):
+
+    def __init__(self):
+
+        super(PunchEnv, self).__init__()
+        # Define action and observation space
+        # They must be gym.spaces objects
+        # Example when using discrete actions:
+        # self.action_space = spaces.Discrete(N_DISCRETE_ACTIONS)
+        self.action_space = spaces.Discrete(2)
+
+        self.observation_space = spaces.Box(
+            low=-1.0, high=1.0, shape=(20,), dtype=float)
+
+        # todo this is linear speed, add acceleration
+        self.shoulder_angle = np.linspace(10, 86, 100)
+        self.elbow_angle = np.linspace(110, 0, 100)
+        self.motion_idx = 0
+        self.direction = 1
+
+        self.model = mujoco.MjModel.from_xml_string(arm_xml)
+        self.data = mujoco.MjData(self.model)
+
+        self.previous_contact_state = False
+        self.current_contact_state = False
+        self.ncon = 0
+
+        print("__init__ called")
+
+    def __del__(self):
+        print("__del__ called")
+
+    def step(self, action):
+
+        # print(self.data.qpos)
+        # print(self.data.qvel)
+
+        mujoco.mj_step(self.model, self.data)
+
+        # joint manipulation start
+        q = quaternions.axangle2quat(
+            [0, 1, 0], math.radians(self.shoulder_angle[self.motion_idx]), is_normalized=True)
+
+        self.data.qpos[0] = q[0]  # w
+        self.data.qpos[1] = q[1]  # x
+        self.data.qpos[2] = q[2]  # y
+        self.data.qpos[3] = q[3]  # z
+
+        self.data.qpos[4] = math.radians(
+            self.elbow_angle[self.motion_idx])  # w
+        self.data.qvel[3] = 0
+        # print(data.qvel)
+
+        if action == 1:
+            self.motion_idx += 1
+        elif action == 0:
+            self.motion_idx -= 1
+
+        if self.motion_idx >= len(self.elbow_angle) - 1:
+            self.motion_idx = len(self.elbow_angle) - 1
+        elif self.motion_idx <= 0:
+            self.motion_idx = 0
+        # joint manipulation end
+
+        # assemble observation
+        # cartesian position of upper arm, lower arm, hand and ball
+        # quaternion of upper arm, lower arm
+        upper_arm_p = normalize(
+            self.data.geom_xpos[self.model.geom('upper_arm').id])
+        lower_arm_p = normalize(
+            self.data.geom_xpos[self.model.geom('lower_arm').id])
+        hand_p = normalize(self.data.geom_xpos[self.model.geom('hand').id])
+        ball_p = normalize(self.data.geom_xpos[self.model.geom('ball').id])
+
+        upper_arm_r = normalize(quaternions.mat2quat(
+            self.data.geom_xmat[self.model.geom('upper_arm').id]))
+        lower_arm_r = normalize(quaternions.mat2quat(
+            self.data.geom_xmat[self.model.geom('lower_arm').id]))
+
+        # concatenate numpy arrays
+        observation = np.concatenate(
+            (upper_arm_p, lower_arm_p, hand_p, ball_p, upper_arm_r, lower_arm_r), axis=None)
+
+        # print('--------------- observation')
+        # print(observation)
+
+        # reward is
+
+        # print('--------------- acc')
+        # acceleration is of shape (10,) 3 for shoulder ball joint, 1 for elbow hinge joint, 6 for sphere free joint
+        reward = np.sum(self.data.qacc[4:])
+
+        if self.data.ncon > 0:
+            self.current_contact_state = True
+
+        if self.previous_contact_state == False and self.current_contact_state == True:
+            self.ncon += 1
+
+        self.previous_contact_state = self.current_contact_state
+
+        # when contact twice, done
+        if self.ncon >= 2:
+            done = True
+        else:
+            done = False
+
+        truncate = False
+        info = {}
+
+        return observation, reward, done, truncate, info
+
+    def reset(self, seed=None, options=None):
+        super().reset(seed=seed, options=options)
+
+        mujoco.mj_resetData(self.model, self.data)
+
+        self.previous_contact_state = False
+        self.current_contact_state = False
+        self.ncon = 0
+
+        observation = np.zeros(20, dtype=np.float32)
+
+        self.reward = 0
+
+        # Implement reset method
+        info = {}
+        return observation, info
+
+
+if __name__ == "__main__":
+
+    env = PunchEnv()
+
+    check_env(env)
--- arm.py
+++ arm.py
@@ -9,87 +9,7 @@ from transforms3d import quaternions
 import matplotlib.pyplot as plt
 # from dm_control.mujoco.wrapper.mjbindings import mjlib
 
-
-tall = 1.6
-
-left_shouder = -0.4
-right_shouder = 0.4
-
-target_x = -1.1
-target_y = 0.52
-target_z = 1.4
-
-upper_arm_l = 0.4
-lower_arm_l = 0.4
-hand_l = 0.14
-
-skin_color = '0.8 0.6 0.4 1'
-
-string_length = 0.4
-anchor_z = target_z + string_length
-
-xml = """
-<mujoco>
-    <asset>
-        <texture name="grid" type="2d" builtin="checker" rgb1=".1 .2 .3"
-        rgb2=".2 .3 .4" width="300" height="300"/>
-        <material name="grid" texture="grid" texrepeat="8 8" reflectance=".2"/>
-    </asset>
-    <worldbody>
-        
-        <light pos="0 0 2."/>
-        
-        <geom size="1 1 .01" type="plane" material="grid"/>
-        
-        <site name="anchor" pos="{target_x} {target_y} {anchor_z}" size=".01"/>
-
-        <body pos="0 0 0">
-            <geom name="spine" type="box" fromto="0 0 0. 0 0 {tall}" size=".1" 
-                pos="0 0 0" rgba="1 1 1 1"/>
-            <geom name="shoulder" type="box" fromto="0 {left_shouder} {tall} 0 {right_shouder} {tall}" size=".1" 
-                pos="0 0 0" rgba="1 1 1 1"/>
-
-            <!-- Shoulder joint -->
-            <body pos="0. 0.55 {tall}" gravcomp="1">
-                <joint name="shoulder" type="ball" pos="0 0 0"/>
-                <geom name="upper_arm" type="cylinder" fromto="0 0. 0. 0 0. -{upper_arm_l}" size="0.1" 
-                rgba="{skin_color}"/>
-
-                <!-- Elbow joint -->
-                <body pos="0. 0. -0.6" gravcomp="1">
-                    <joint name="elbow" type="hinge" pos="0 0 0.1" axis="0 1 0"/>
-                    <geom name="lower_arm" type="cylinder"  fromto="0 0 0. 0 0 -{lower_arm_l}" size=".08" 
-                    pos="0 0 0" rgba="{skin_color}"/>
-
-                    <!-- Hand -->
-                    <body pos="0. 0. -{lower_arm_l}" gravcomp="1">
-                        <geom name="hand" type="cylinder" fromto="0 0 0.0 0 0 -{hand_l}" size="0.09" 
-                        pos="0 0 0" rgba="{skin_color}"/>
-                    </body>
-                </body>
-            </body>
-        </body>
-
-        <body name="target" pos="{target_x} {target_y} {target_z}">
-            <joint name="free" type="free"/>
-            <geom name="ball" type="sphere" size=".06" rgba="1 0 0 1"/>
-            <site name="hook" pos="0. 0. 0." size=".01"/>
-        </body>
-
-    </worldbody>
-
-    <tendon>
-        <spatial name="wire" limited="true" range="0 {string_length}" width="0.003">
-        <site site="anchor"/>
-        <site site="hook"/>
-        </spatial>
-    </tendon>
-</mujoco>
-""".format(tall=tall, left_shouder=left_shouder, right_shouder=right_shouder,
-           upper_arm_l=upper_arm_l, lower_arm_l=lower_arm_l, hand_l=hand_l, skin_color=skin_color,
-           target_x=target_x, target_y=target_y, target_z=target_z,
-           anchor_z=anchor_z, string_length=string_length)
-
+from xml import arm_xml
 
 def plot_force(times, forces):
     lines = plt.plot(times, forces)
@@ -310,8 +230,8 @@ class ArmSim:
             # reward is 
             # 
 
-            print('---------------')
-            # print(self.data.geom_xpos)
+            print('--------------- acc')
+            print(self.data.qacc[4:])
 
             upper_arm_p = self.data.geom_xpos[self.model.geom('upper_arm').id]
             lower_arm_p = self.data.geom_xpos[self.model.geom('lower_arm').id]
@@ -321,13 +241,10 @@ class ArmSim:
             upper_arm_r = quaternions.mat2quat(self.data.geom_xmat[self.model.geom('upper_arm').id])
             lower_arm_r = quaternions.mat2quat(self.data.geom_xmat[self.model.geom('lower_arm').id])
 
-            # observation = upper_arm_p + lower_arm_p + hand_p + ball_p + upper_arm_r + lower_arm_r
-
-            print(upper_arm_p, lower_arm_p, hand_p, ball_p, upper_arm_r, lower_arm_r)
-
             # concatenate numpy arrays
             observation = np.concatenate((upper_arm_p, lower_arm_p, hand_p, ball_p, upper_arm_r, lower_arm_r), axis=None)
 
+            print('--------------- observation')
             print(observation)
 
             # print(observation)
@@ -404,7 +321,7 @@ if __name__ == "__main__":
         # create dir img
         os.mkdir('img')
 
-    rnder = ArmSim(xml)
+    rnder = ArmSim(arm_xml)
 
-    rnder.run()
+    rnder.run(10)
     # rnder.run_with_viewer()
